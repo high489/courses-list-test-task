45b82dbdbf0547f5031a6a49159f5749975560a4
high489 <vitalii.matskaniuk@gmail.com>
2025-12-02 16:37:43 -0300

    Initial commit
    Add: Project structure with Vite. Design system from Figma

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './css/main.scss'
+
+document.querySelector('#app').innerHTML = `
+  <div class='courses-list'>
+    <h6 class='courses-list__subtitle'>ENJOY YOUR STUDYING!</h6>
+    <h1 class='courses-list__title'>Our cources online</h1>
+  </div>
+`
